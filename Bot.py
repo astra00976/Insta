dd5b9f21f20083bb5336e6a720a37fd8e4ed031c
astra00976 <grscalsip@indogmail.com>
2026-03-23 00:49:02 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -179,12 +179,11 @@ def scrape_background(job, context):
 
             if not job.running:
                 break
-            log("Scanning page for posts...")
             links = page.evaluate("""
-                Array.from(document.querySelectorAll('a'))
-                    .map(a => a.href)
-                    .filter(h => h.includes('/p/') || h.includes('/reel/'))
-            """)
+              Array.from(document.querySelectorAll('article a'))
+             .map(a => a.href)
+             .filter(h => /\\/(p|reel)\\//.test(h))
+           """)
 
             new_posts = 0
 
